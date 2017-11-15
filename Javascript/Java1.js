aa77d899a05ff94c2c110364070d8c64e446f96d
Robindekogel <31471377+Robindekogel@users.noreply.github.com>
2017-11-15 12:41:15 +0100

    Add files via upload

---
+++ Javascript/Java1.js
@@ -0,0 +1,14 @@
+var naam = {
+    voornaam: "Robin",
+    tussenvoegsel: "de",
+    achternaam: "Kogel"
+
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log("i heeft de waarde van " + i);
+}
+
+console.log(naam.voornaam);
+console.log(naam.tussenvoegsel);
+console.log(naam.achternaam);
