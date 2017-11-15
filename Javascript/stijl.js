6d8900f1844b24e858fcaff0a07e4209b4285557
Robindekogel <31471377+Robindekogel@users.noreply.github.com>
2017-11-15 13:29:33 +0100

    Add files via upload

---
+++ Javascript/stijl.js
@@ -0,0 +1,6 @@
+document.getElementById("bl1").style.backgroundColor = "aqua";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.marginTop = "25px";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.marginTop = "50px";
+document.getElementById("bl4").style.backgroundColor = "red";
