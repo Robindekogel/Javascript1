c552da3115d2d5f4e0b941219efde98a6c93076f
Robindekogel <31471377+Robindekogel@users.noreply.github.com>
2017-11-15 12:48:53 +0100

    Add files via upload

--- Javascript/Java1.js
+++ Javascript/Java1.js
@@ -5,10 +5,21 @@ var naam = {
 
 }
 
+var auto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "Blauw"
+}
+
 for (var i = 0; i < 10; i++) {
     console.log("i heeft de waarde van " + i);
 }
 
+console.log(auto.merk);
+console.log(auto.type);
+console.log(auto.aantalWielen);
+console.log(auto.kleur);
 console.log(naam.voornaam);
 console.log(naam.tussenvoegsel);
 console.log(naam.achternaam);
